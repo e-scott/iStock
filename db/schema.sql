6294a2b0ec817fad9608c34213083518d9fdff5a
e-scott <ejohnson@rojoli.com>
2019-05-31 21:20:46 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,13 @@ CREATE TABLE `stocks` (
     updatedAt datetime,
 	PRIMARY KEY ( `id` )
 );
+
+CREATE TABLE IF NOT EXISTS iStock_users(
+   id              INT(5) AUTO_INCREMENT NOT NULL,
+   first_name      VARCHAR(100) NOT NULL,
+   last_name       VARCHAR(100) NOT NULL,
+   username        VARCHAR(100) NOT NULL,
+   password        VARCHAR(255) NOT NULL,
+   CONSTRAINT pk_users PRIMARY KEY(id)
+)ENGINE='InnoDB' CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
